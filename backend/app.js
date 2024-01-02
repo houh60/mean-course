c14b4fcf96c12a7edb96a59329bb95e7998adcae
William Hou <williamhou38@yahoo.com>
2024-01-02 13:34:47 -0500

    new change

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-
-const Post = require('./models/post');
+const postRoutes = require('./routes/posts');
 
 const app = express();
-
-mongoose.connect('mongodb+srv://@cluster0.gkvfa.mongodb.net/MEAN?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://William:nKCuqEUG4Mtk9AwW@cluster0.gkvfa.mongodb.net/MEAN?retryWrites=true&w=majority')
   .then(() => {
     console.log('Connected to database!');
   })
@@ -18,8 +16,6 @@ mongoose.connect('mongodb+srv://@cluster0.gkvfa.mongodb.net/MEAN?retryWrites=tru
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const posts = [];
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
@@ -27,54 +23,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    date: req.body.date
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Posts added successfully!',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({ _id: req.params.id }, post).then(() => {
-    res.status(200).json({ message: 'Update successful!' });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: 'Posts fetched successfully!',
-      posts: documents
-    });
-  });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found!' });
-    }
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(() => {
-    res.status(200).json({ message: 'Post deleted!' });
-  });
-});
+app.use('/api/posts', postRoutes);
 
 module.exports = app;
